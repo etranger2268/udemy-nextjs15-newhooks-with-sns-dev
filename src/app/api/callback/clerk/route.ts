ca0bb06a23619631eb2bab093c7319126ed3828d
etranger2268 <shun.5.2268@gmail.com>
2026-01-26 02:00:01 +0900

    develop-2-14

--- src/app/api/callback/clerk/route.ts
+++ src/app/api/callback/clerk/route.ts
@@ -1,16 +1,39 @@
 import { verifyWebhook } from '@clerk/nextjs/webhooks';
-import type { NextRequest } from 'next/server';
+import { type NextRequest, NextResponse } from 'next/server';
+import { prisma } from '@/lib/prisma';
 
 export async function POST(req: NextRequest) {
   try {
     const evt = await verifyWebhook(req);
+    const eventType = evt.type;
 
     // Do something with payload
     // For this guide, log payload to console
-    const { id } = evt.data;
-    const eventType = evt.type;
-    console.log(`Received webhook with ID ${id} and event type of ${eventType}`);
-    console.log('Webhook payload:', evt.data);
+
+    if (eventType === 'user.created') {
+      try {
+        const { id, email_addresses, username, image_url } = evt.data;
+        const email = email_addresses?.[0]?.email_address;
+
+        if (!id || !email) {
+          throw new Error();
+        }
+
+        await prisma.user.create({
+          data: {
+            clerkId: id,
+            email: email,
+            username: username ?? email.split('@')[0] ?? id,
+            name: username || 'Unknown',
+            image: image_url,
+          },
+        });
+        return NextResponse.json({ message: 'ユーザーの作成に成功しました' });
+      } catch (error) {
+        console.error('Create User ERROR:', error);
+        return NextResponse.json({ message: 'ユーザーの作成に失敗しました' }, { status: 500 });
+      }
+    }
 
     return new Response('Webhook received', { status: 200 });
   } catch (err) {
